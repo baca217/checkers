2b7752ec6a99602726ff1d31a12ff79e3bc791db
time <tieu5230@colorado.edeu>
2020-12-02 21:22:35 -1000

    addedLogin. Can't Launch Board

--- src/checkersGame.java
+++ src/checkersGame.java
@@ -1,10 +1,23 @@
 import checkers.*;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
-public class checkersGame
+public class checkersGame extends Application
 {
-    public static void main(String[] args)
-    {
-        CheckersApp app = new CheckersApp();
-        app.launch(args);
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("login.fxml"));
+        primaryStage.initStyle(StageStyle.UNDECORATED);
+        primaryStage.setScene(new Scene(root, 600, 400));
+        primaryStage.show();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
     }
 }
---
+++ src/login/LoginController.java
@@ -0,0 +1,63 @@
+package login;
+
+import checkers.CheckersApp;
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.stage.Stage;
+
+import java.io.File;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+
+public class LoginController implements Initializable {
+    @FXML
+    public Button exitButton;
+
+    @FXML
+    public ImageView checkerBoardView;
+
+    @FXML
+    public Label invalidNameText;
+
+    @FXML
+    public Button StartButton;
+
+    @FXML
+    public TextField player2Input;
+
+    @FXML
+    public TextField player1Input;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        File checkerBoardFile = new File("Images/MockUI.png");
+        Image checkerBoardImage = new Image(checkerBoardFile.toURI().toString());
+        checkerBoardView.setImage(checkerBoardImage);
+    }
+
+    public void leaveLoginPage(ActionEvent event) {
+        Stage stage = (Stage) exitButton.getScene().getWindow();
+        stage.close();
+    }
+
+    public void startGame(ActionEvent event) {
+        if(checkInvalidNames(player1Input.getText(), player2Input.getText())) {
+            invalidNameText.setText("Invalid Names :( !!");
+            return;
+        }
+
+        Application.launch(CheckersApp.class);
+    }
+
+    public boolean checkInvalidNames(String name1, String name2) {
+        return name1.length() <= 0 || name2.length() <= 0 || name1.equals(name2);
+    }
+}
